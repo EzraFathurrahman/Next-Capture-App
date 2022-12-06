ad44dbc884966057c947625cdf2ee30e9a5a7ce8
Ezraft <55395086+EzraFathurrahman@users.noreply.github.com>
2022-12-06 19:15:14 +0700

    deploy capture App

---
+++ components/SliderData.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const SliderData =[
+  {
+    image : 'https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG9ncmFwaHl8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=1100&q=60'
+  },
+{
+  image:' https://images.unsplash.com/photo-1486916856992-e4db22c8df33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBob3RvZ3JhcGh5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1100&q=60'
+},
+{
+  image:' https://images.unsplash.com/photo-1500051638674-ff996a0ec29e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBob3RvZ3JhcGh5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1100&q=60'
+},
+{
+  image:' https://images.unsplash.com/photo-1565004569929-4f400c11e9ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBob3RvZ3JhcGh5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1100&q=60'
+},
+{
+  image:'https://images.unsplash.com/photo-1657214059493-986710bc4788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MTV8fHBob3RvZ3JhcGh5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1100&q=60'
+},
+{
+  image:'https://images.unsplash.com/photo-1488926756850-a13b25e2f415?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80'
+}
+
+]
+
